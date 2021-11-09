654f0e3898f5221cfb682e60b9f5578350171968
Larsenik67 <ylachqar@gmail.com>
2021-11-09 16:04:11 +0100

    Ajout CSS

--- index.php
+++ index.php
@@ -4,33 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Ajouter un produit</title>
 </head>
     <body>
-        
-        <h1>Ajouter un produit</h1>
-        <form action ="traitement.php" method="post">
-            <p>
-                <label>
-                    Nom du produit :
-                    <input type="text" name="name">
-                </label>
-            </p>
-            <p>
-                <label>
-                    Prix du produit :
-                    <input type="number" name="price">
-                </label>
-            </p>
-            <p>
-                <label>
-                    Quantité désirée :
-                    <input type="number" name="qtt" value="1">
-                </label>
-            </p>
-            <p>
-                <input type="submit" name="submit" value="Ajouter le produit">
-            </p>
-        </form>
+        <div class ="my-5">
+            <div class="formulaire">
+                <h1 class="my-2">Ajouter un produit<hr></h1>
+                <form action ="traitement.php" method="post">
+                    <p>
+                        <label class="my-1">
+                            Nom du produit :
+                            <input type="text" name="name">
+                        </label>
+                    </p>
+                    <p>
+                        <label class="my-1">
+                            Prix du produit :
+                            <input type="number" name="price">
+                        </label class="my-1">
+                    </p>
+                    <p>
+                        <label class="my-1">
+                            Quantité désirée :
+                            <input type="number" name="qtt" value="1">
+                        </label>
+                    </p>
+                    <p>
+                        <input class="my-1, submit" type="submit" name="submit" value="Ajouter le produit">
+                    </p>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
